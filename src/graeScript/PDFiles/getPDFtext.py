c72c1deb261d464f43108f8b1ed62436c1d77913
greysk <83403671+greysk@users.noreply.github.com>
2022-02-02 04:53:31 -0500

    Remove one-off codes.
    Delete colorsToExcel.py which was a one-off code.
    Add functionality to getPDFtext.py beyond extracting 1 pg from PDF.

--- src/graeScript/PDFiles/getPDFtext.py
+++ src/graeScript/PDFiles/getPDFtext.py
@@ -3,11 +3,43 @@ import fitz
 from graeScript import outfile_path
 
 
-def getPDFtext(file, outfile_name):
-    doc = fitz.open(file)
-    outfile = open(outfile_path() / outfile_name + '.txt', 'wb')
-    for page in doc:
-        text = page.get_text("html").encode("utf8")
-        outfile.write(text)
-        outfile.write(bytes((12,)))
-    outfile.close
+class PdfContent:
+    file_modes = ('text', 'html', 'json', 'rawjson', 'xhtml', 'xml')
+    non_file_modes = ('blocks', 'dict', 'rawdict', 'words')
+
+    def __init__(self, file) -> None:
+        self.file = file
+        doc = fitz.open(file)
+        self.page_num = doc.page_count
+        self.toc = doc.get_toc()
+        self.metadata = doc.metadata
+        doc.close()
+
+    def to_file(self, outfile, *, mode='text', pgstart=1,
+                pgstop=None, pgstep=1):
+        if mode not in self.file_modes:
+            print(f'Mode must be one of {", ".join(self.file_modes)}.')
+            raise SystemExit
+        doc = fitz.open(self.file)
+        if not pgstop:
+            pgstop = doc.page_count
+        with open(outfile_path() / outfile, 'wb') as f:
+            for page in doc.pages(pgstart, pgstop, pgstep):
+                text = page.get_text(mode).encode('utf8')
+                f.write(text)
+                f.write(bytes((12,)))
+        doc.close()
+
+    def get(self, *, mode='text', pgstart=1, pgstop=None, pgstep=1):
+        if mode not in self.file_modes and mode not in self.non_file_modes:
+            print(f'Mode must be one of {", ".join(self.file_modes)},'
+                  f' {", ".join(self.non_file_modes)}.')
+            raise SystemExit
+        doc = fitz.open(self.file)
+        if not pgstop:
+            pgstop = doc.page_count
+        text = []
+        for page in doc.pages(pgstart, pgstop, pgstep):
+            text.append(page.get_text(mode))
+        doc.close()
+        return text
