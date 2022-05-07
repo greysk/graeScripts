c86126638aab241a992912a1466d4f83d071c1d6
greysk <83403671+greysk@users.noreply.github.com>
2022-05-07 01:29:32 -0400

    Add Enum and documentation Enum subclass added for setting output pdf text mode. Add docstrings.

--- src/graeScript/PDFiles/get_pdf_content.py
+++ src/graeScript/PDFiles/get_pdf_content.py
@@ -1,13 +1,43 @@
 '''Extracting Text from PDF'''
+import enum
+from pathlib import Path
 import fitz
 from graeScript import outfile_path
+
+
+class FileMode(enum):
+    """
+    PDF output text modes that work with sending to file.
+
+    From Fitz/pymupdf:
+        https://pymupdf.readthedocs.io/en/latest/page.html?highlight=get_text#Page.get_text
+    """
+    text = 0
+    html = 1
+    json = 2
+    rawjson = 3
+    xhtml = 4
+    xml = 5
+
+
+class NonFileMode(enum):
+    """
+    PDF output text modes that do not work with sending to file.
+
+    From Fitz/pymupdf:
+        https://pymupdf.readthedocs.io/en/latest/page.html?highlight=get_text#Page.get_text
+    """
+    blocks = 0
+    dict = 1
+    rawdict = 2
+    words = 3
 
 
 class PdfContent:
     file_modes = ('text', 'html', 'json', 'rawjson', 'xhtml', 'xml')
     non_file_modes = ('blocks', 'dict', 'rawdict', 'words')
 
-    def __init__(self, file) -> None:
+    def __init__(self, file: str | Path) -> None:
         self.file = file
         doc = fitz.open(file)
         self.page_num = doc.page_count
@@ -15,8 +45,26 @@ class PdfContent:
         self.metadata = doc.metadata
         doc.close()
 
-    def to_file(self, outfile, *, mode='text', pgstart=1,
-                pgstop=None, pgstep=1):
+    def to_file(self, outfile: str | Path, *, mode: str | FileMode = 'text',
+                pgstart: int = 1, pgstop: int = None, pgstep: int = 1):
+        """
+        Send PDF text/contents to output file in the format of `mode`.
+
+        Args:
+            outfile (str | Path): The output file path.
+            mode (str | FileMode, optional): The mode of the PDF output.
+                Defaults to 'text'. See Fitz docs: https://bit.ly/3FiUbHf
+            pgstart (int, optional): The page at which to start. Defaults to 1.
+            pgstop (int, optional): The page at which to stop. Defaults to
+                None.
+            pgstep (int, optional): The page steps. Defaults to 1 which
+                outputs every page from pgstart to pgstop.
+
+        Raises:
+            SystemExit: If the mode is not a valid file mode.
+        """
+        if isinstance(mode, (FileMode)):
+            mode = mode.name
         if mode not in self.file_modes:
             print(f'Mode must be one of {", ".join(self.file_modes)}.')
             raise SystemExit
@@ -30,7 +78,26 @@ class PdfContent:
                 f.write(bytes((12,)))
         doc.close()
 
-    def get(self, *, mode='text', pgstart=1, pgstop=None, pgstep=1):
+    def get(self, *, mode: str | FileMode | NonFileMode = 'text',
+            pgstart: int = 1, pgstop: int = None, pgstep: int = 1):
+        """
+        Return PDF text/contents in the format of `mode`.
+
+        Args:
+            mode (str | FileMode | NonFileMode, optional): The mode of the PDF
+                output. Defaults to 'text'. See Fitz docs:
+                https://bit.ly/3FiUbHf
+            pgstart (int, optional): The page at which to start. Defaults to 1.
+            pgstop (int, optional): The page at which to stop. Defaults to
+                None.
+            pgstep (int, optional): The page steps. Defaults to 1 which
+                outputs every page from pgstart to pgstop.
+
+        Raises:
+            SystemExit: If mode is not a FileMode or NonFileMode
+        """
+        if isinstance(mode, (FileMode, NonFileMode)):
+            mode = mode.name
         if mode not in self.file_modes and mode not in self.non_file_modes:
             print(f'Mode must be one of {", ".join(self.file_modes)},'
                   f' {", ".join(self.non_file_modes)}.')
@@ -43,3 +110,7 @@ class PdfContent:
             text.append(page.get_text(mode))
         doc.close()
         return text
+
+
+if __name__ == '__main__':
+    pass
